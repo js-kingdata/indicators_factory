aa2ec7893e003da723d0f4d3864ee293ea934941
hejian991 <hejian991@126.com>
2022-12-15 00:12:34 +0800

    1、可运行： scrapy crawl idx-btc-arh999 scrapy crawl idx-btc-mvrv
    2、去除 token、internal_api的调用
    
    3、去除 多余的依赖
    
    4、scrapy crawl idx-contract-position-ratio，运行 print 正常，但有异常抛出

---
+++ crawlers/indicators/spiders/mainstream_coin_long_short_ratio/new_coin_spider.py
@@ -0,0 +1,53 @@
+import requests
+
+from crawlers.utils import SpiderBase
+from jinja2 import Template
+
+
+def get_coin_info(coin):
+    response = requests.get(url=f'https://fapi.coinglass.com/api/futures/longShortRate?symbol={coin}&timeType=3')
+    data = response.json()['data'][0]
+    return {
+        'symbol': data['symbol'],
+        'long_short_rate': round(data['longRate'] / data['shortRate'], 2),
+        'long_rate': data['longRate'],
+        'short_rate': data['shortRate'],
+        'list': [{
+            'exchange_name': exchange_info['exchangeName'],
+            'long_rate': exchange_info['longRate'],
+            'short_rate': exchange_info['shortRate']
+        } for exchange_info in data['list'][:3]]
+    }
+
+
+class ContractPositionRatio(SpiderBase):
+    def parse(self, response, **kwargs):
+        pass
+
+    name = 'idx-contract-position-ratio'
+    all_coin = ['BTC']
+
+    def start_requests(self):
+        params = {
+            'info': [get_coin_info(coin) for coin in self.all_coin]
+        }
+        print(Template(self.alert_en_template()).render(params))
+        print(Template(self.alert_cn_template()).render(params))
+
+    # must be declare
+    def alert_en_template(self):
+        return """
+According to KingData monitoring, {% for coin in info%}In the past 4 hours, {{coin.symbol}} Long/Short Ratio across network is {{coin.long_short_rate}}, with {{coin.long_rate}}% longs and  {{coin.short_rate}}% shorts. {%if coin.long_short_rate>1%}Longs outweigh shorts{% else %}Shorts outweigh Longs{% endif %}.
+Among leading exchanges:{% for exchange in coin.list %}
+{{exchange.exchange_name}}: long {{exchange.long_rate}}%, short {{exchange.short_rate}}%{% endfor %}
+{% endfor %}
+"""
+
+    # must be declare
+    def alert_cn_template(self):
+        return """
+据 KingData 数据监控，{% for coin in info%}最近4小时，{{coin.symbol}}全网合约多空比为 {{coin.long_short_rate}}，多单占比 {{coin.long_rate}}%，空单占比{{coin.short_rate}}%，{%if coin.long_short_rate>1%}看多人数大于看空人数{% else %}看空人数大于看多人数{% endif %}。
+其中主流交易所：{% for exchange in coin.list %}
+{{exchange.exchange_name}}：做多{{exchange.long_rate}}% 做空{{exchange.short_rate}}%{% endfor %}
+{% endfor %}
+"""
